eebc7102cd863e6bcf2b7a6115cba173529a8c0e
= <=>
2023-02-23 19:45:04 +0100

    update

--- public-web/js/nav.js
+++ public-web/js/nav.js
@@ -9,11 +9,18 @@ const createNav = () => {
                     <input type="text" class="search-box" placeholder="Zoek Merk, Product">
                     <button class="search-btn">Zoek</button>
                 </div>
-                <a href="#"><img src="img/cart.png" alt=""></a>
-                <a href="signup.html"><img src="img/user.png" alt=""></a>
-            </div>
+                <a href="#" class="toggle-button"> 
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>
+                <a href="#" class="cart-img"><img src="img/cart.png" alt=""></a>
+                <a href="signup.html" class="pf-img"><img src="img/user.png" alt=""></a>
+                
+             </div>
         </div>
         
+
         <ul class="links-container">
         <li class="link-item"><a href="./index.html" class="link">Home</a></li>
         <li class="link-item"><a href="./kabels.html" class="link">Kabels</a></li>
@@ -22,13 +29,6 @@ const createNav = () => {
         <li class="link-item"><a href="./stekkerdozen.html" class="link">Stekkerdozen</a></li>
         </ul>
         
-        <ul class="mobile-container">
-                <li class="mobile-link-item"><a href="./index.html" class="link">Home</a></li>
-                <li class="mobile-link-item"><a href="./kabels.html" class="link">Kabels</a></li>
-                <li class="mobile-link-item"><a href="./adapters.html" class="link">Adapters</a></li>
-                <li class="mobile-link-item"><a href="./draadloos.html" class="link">Draadloos</a></li>
-                <li class="mobile-link-item"><a href="./stekkerdozen.html" class="link">Stekkerdozen</a></li>
-        </ul>
     `;
 }
 
